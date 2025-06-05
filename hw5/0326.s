60f3b01f2862ea179d165a2860b8a9cc29e8bb51
yawem0525 <161785938+yawem0525@users.noreply.github.com>
2025-06-06 04:01:41 +0800

    Create 0326.s

---
+++ hw5/0326.s
@@ -0,0 +1,48 @@
+	.file	"0326.c"
+	.text
+	.globl	mul2
+	.def	mul2;	.scl	2;	.type	32;	.endef
+	.seh_proc	mul2
+mul2:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	movl	%edx, 24(%rbp)
+	movl	16(%rbp), %eax
+	imull	24(%rbp), %eax
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "result=%d\12\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	call	__main
+	movl	$2, %edx
+	movl	$5, %ecx
+	call	mul2
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (x86_64-posix-seh-rev0, Built by MinGW-W64 project) 8.1.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
